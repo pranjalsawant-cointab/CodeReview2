0859d1fb996d50575cfb124cd4260ff8532723ca
pranjalsawant-cointab <pranjal.sawant@cointab.net>
2025-12-30 15:23:38 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function processData(data) {
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data.length; j++) {
-      if (data[i] === data[j]) {}
+      if (data[i] === data[j]) {
+        continue;
+      }
     }
   }
 }
